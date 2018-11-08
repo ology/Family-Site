537cfd48a845d405a7160b2b50052ecf2db7ebad
Gene Boggs <gene.boggs@gmail.com>
2018-11-08 11:31:43 -0800

    Handy

---
+++ bin/new_user.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Crypt::SaltedHash;
+use Family::Site::Schema;
+use Term::ReadKey;
+
+my $db   = shift || die usage();
+my $user = shift || die usage();
+
+my $schema = Family::Site::Schema->connect( "dbi:mysql:dbname=$db", 'root', 'abc123' );
+
+# Show all users:
+#my @rs = $schema->resultset('User')->search();
+#print $_->username, "\n" for @rs;
+#exit;
+
+my $entry = $schema->resultset('User')->find( { username => $user } );
+
+if ( $entry ) {
+    die "User $user already present";
+}
+else {
+    ReadMode('noecho');
+    print "Password for $user user: ";
+    my $pass = ReadLine(0);
+    chomp $pass;
+    print "\n";
+
+    my $csh = Crypt::SaltedHash->new( algorithm => 'SHA-1' );
+    $csh->add($pass);
+    my $encrypted = $csh->generate;
+
+    my $entry = $schema->resultset('User')->create({ username => $user });
+
+    $entry->password($encrypted);
+    $entry->active(1);
+    $entry->update;
+}
+
+sub usage {
+    "Usage: perl $0 database username";
+}
