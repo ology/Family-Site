7164e4c1ec5451fbd63c1685b404fd500558954b
Gene Boggs <gene.boggs@gmail.com>
2018-11-16 16:22:54 -0800

    Support mp3 uploads

--- lib/Family/Site.pm
+++ lib/Family/Site.pm
@@ -753,7 +753,7 @@ post '/upload' => require_login sub {
 
         # Add a post to the chat about this upload.
         if ( params->{notify} ) {
-            my $src = $name =~ /\.pdf$/i ? '/images/pdf.png' : "album/$name";
+            my $src = $name =~ /\.pdf$/i ? '/images/pdf.png' : $name =~ /\.mp3$/i ? '/images/audio.png' : "album/$name";
             my $text = sprintf
                 '%s %s: Uploaded: <a href="album/%s"><img src="%s" height="10%" width="10%" class="vmid" /></a>',
                 $user->{username},
